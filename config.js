4ad875b5b76961c74ead6157552d3beb3143b40b
tphammm <86755310+tphammm@users.noreply.github.com>
2022-08-04 09:59:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'bxa uiii ',
-    introDesc: `Nhìn em như con ma ế nhưng nhìn kĩ thì a mê á =)))))) `,
-    btnIntro: 'press here for more information  ',
-    title: 'Do you know CPR? 😙',
-    desc: ' Because you are taking my breath away  ',
-    btnYes: 'dĩ nhiên là emm ròiii <33',
-    btnNo: 'không phải em :3',
-    question:'emm có iu anh khommm <3 ',
+    introTitle: 'chịiiii ơiii ',
+    introDesc: `Hứa kbh like bậy bạ nữaa `,
+    btnIntro: 'Click here plsss ',
+    title: 'còn giận hong dạaa ?? 😙',
+    desc: ' Đừng có giận nữaaa màaaaa  ',
+    btnYes: 'hết gòi <33',
+    btnNo: 'chim cút :3',
+    question:' đừng có giận emm nữaaa nhaaa ',
     btnReply: 'send ',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'biết ngayy liu liu  🥰. iu bxaa nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 8h call nhó công chúa.',
+    reply: 'hết giận gòi yên tâmm',
+    mess: 'biết ngayy liu liu  🥰. iu iu 😘😘',
+    messDesc: 'Khi nào đi chơi dìa chở chị đi ăn nhaa nên kco đc giậnnn.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/pham.thong.14473' 
+    messLink: 'https://www.youtube.com/shorts/7gjgawCGtBE?app=desktop' 
 }
